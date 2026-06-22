a1e7b92f080368f9d2fd142f51376dac39b818a7
youssef errachid <yousseferrachid91@gmail.com>
2026-06-22 18:32:13 +0100

    add the function and the style of the back to top button

--- script.js
+++ script.js
@@ -94,3 +94,21 @@ form.addEventListener('submit', function (e) {
     form.reset();
   }
 });
+
+
+const backToTopBtn = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add('show');
+  } else {
+    backToTopBtn.classList.remove('show');
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
